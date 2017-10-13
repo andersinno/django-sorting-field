96cae252a6f615a219ae9241ee0e2a3b21b56c53
Tuomas Suutari <tuomas.suutari@anders.fi>
2017-10-13 10:20:29 +0300

    Make SortingFormField.items a list
    It is safer to be a list, since the generator will be empty after it is
    consumed once.

--- django_sorting_field/fields.py
+++ django_sorting_field/fields.py
@@ -21,11 +21,11 @@ class SortingFormField(forms.CharField):
             "widget": SortingWidget(),
             "required": False,
         })
-        self.items = ()
+        self.items = []
         super(SortingFormField, self).__init__(*args, **kwargs)
 
     def populate(self, items):
-        self.items = (SortedItem(item.pk, force_text(item)) for item in items)
+        self.items = [SortedItem(item.pk, force_text(item)) for item in items]
 
     def prepare_value(self, value):
         value = clean_order_json(value)
