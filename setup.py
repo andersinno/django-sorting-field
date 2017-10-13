9ac5d7d209f13197f969006be1e895c5e808a80b
Tuomas Suutari <tuomas.suutari@anders.fi>
2017-10-13 10:03:34 +0300

    Move metadata from setup.py to setup.cfg

--- setup.py
+++ setup.py
@@ -2,9 +2,5 @@ import setuptools
 
 if __name__ == '__main__':
     setuptools.setup(
-        name="django-sorting-field",
-        version="1.0.0",
-        description="Django Sorting Field",
-        packages=setuptools.find_packages(),
-        include_package_data=True,
-    )
+        setup_requires=['setuptools>=34.0', 'setuptools-gitver'],
+        gitver=True)
